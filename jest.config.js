ef2ec769d923edee0bbf710ab2153bc8e2a9cd6d
naathanbrown <nathancjb@googlemail.com>
2024-12-10 14:28:09 +0000

    feat(cb2-15352): complete new test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src', '<rootDir>/tests'],
-  setupFiles: ['jest-plugin-context/setup'],
-  moduleFileExtensions: ['js', 'ts', 'd.ts'],
-  testResultsProcessor: 'jest-sonar-reporter',
-  testMatch: ['**/*.*Test.ts'],
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	roots: ['<rootDir>/src', '<rootDir>/tests'],
+	setupFiles: ['jest-plugin-context/setup'],
+	moduleFileExtensions: ['js', 'ts', 'd.ts'],
+	testResultsProcessor: 'jest-sonar-reporter',
+	testMatch: ['**/*.*Test.ts'],
+	collectCoverageFrom: ['!tests/**'],
 };
--- tests/unit/functions/govNotify.unitTest.ts
+++ tests/unit/functions/govNotify.unitTest.ts
@@ -1,50 +1,101 @@
-import { Context } from 'aws-lambda';
-import "reflect-metadata";
+import { Context, S3EventRecord } from 'aws-lambda';
+import 'reflect-metadata';
 import Container from 'typedi';
+import { EmailRequestProcessor } from '../../../src/functions/EmailRequestProcessor';
 import { handler } from '../../../src/handler';
+import { NotificationService } from '../../../src/services/NotificationService';
 import { S3BucketService } from '../../../src/services/S3BucketService';
 import { S3BucketMockService } from '../../models/S3BucketMockService';
 
 describe('gov-notify', () => {
-  Container.set(S3BucketService, new S3BucketMockService());
+	Container.set(S3BucketService, new S3BucketMockService());
 
-  beforeEach(() => {
-    jest.resetAllMocks();
-  })
+	beforeEach(() => {
+		jest.resetAllMocks();
+		jest.spyOn(NotificationService.prototype, 'initializeNotifyClient').mockResolvedValue(Promise.resolve());
+	});
 
- it('should error if there is no event records', async () => {
-   const event = { foo: 'bar'};
+	it('should error if there is no event records', async () => {
+		const event = { foo: 'bar' };
 
-   expect(async () => {
-    await handler(event, {} as unknown as Context, () => {});
-   }).rejects.toThrow('Event is empty');
- })
+		await expect(async () => {
+			await handler(event, {} as unknown as Context, () => {});
+		}).rejects.toThrow('Event is empty');
+	});
 
- it('should error if the event records are not an array', async () => {
-  const event = { Records: 'bar'};
+	it('should error if the event records are not an array', async () => {
+		const event = { Records: 'bar' };
 
-  expect(async () => {
-   await handler(event, {} as unknown as Context, () => {});
-  }).rejects.toThrow('Event is empty');
- })
+		await expect(async () => {
+			await handler(event, {} as unknown as Context, () => {});
+		}).rejects.toThrow('Event is empty');
+	});
 
-//  it('should call the process method once when there is one valid s3 record', async () => {
-//   expect.assertions(1);
-//   const event = { Records: ['event1', 'event2'] }
+	it('should call the process method once when there is one valid s3 record', async () => {
+		expect.assertions(1);
+		const event = { Records: ['event1'] };
 
-//   jest.spyOn(EmailRequestProcessor.prototype, 'getRecordS3Objects').mockReturnValue(
-//     [{event: 'event1'}] as unknown as S3EventRecord['s3'][]
-//   )
+		jest
+			.spyOn(EmailRequestProcessor.prototype, 'getRecordS3Objects')
+			.mockReturnValue([{ event: 'event1' }] as unknown as S3EventRecord['s3'][]);
 
-//   const processSpy = jest.spyOn(EmailRequestProcessor.prototype, 'process').mockImplementation(() => {
-//     return Promise.resolve();
-//   })
+		const processSpy = jest
+			.spyOn(EmailRequestProcessor.prototype, 'process')
+			.mockImplementation(() => Promise.resolve());
 
-//   await handler(event, {} as unknown as Context, () => {});
+		await handler(event, {} as unknown as Context, () => {});
 
-//   expect(processSpy).toHaveBeenCalledTimes(1);
-//  })
-//  it('should call the process method twice when there is two valid s3 records')
-//  it('should have one partial batch failure when two records but only one fails')
-//  it('should have two partial batch failures when two records and both fail')
+		expect(processSpy).toHaveBeenCalledTimes(1);
+	});
+
+	it('should call the process method twice when there is two valid s3 records', async () => {
+		expect.assertions(1);
+		const event = { Records: ['event1'] };
+
+		jest
+			.spyOn(EmailRequestProcessor.prototype, 'getRecordS3Objects')
+			.mockReturnValue([{ event: 's3Record1' }, { event: 's3Record2' }] as unknown as S3EventRecord['s3'][]);
+
+		const processSpy = jest
+			.spyOn(EmailRequestProcessor.prototype, 'process')
+			.mockImplementation(() => Promise.resolve());
+
+		await handler(event, {} as unknown as Context, () => {});
+
+		expect(processSpy).toHaveBeenCalledTimes(2);
+	});
+
+	it('should have one partial batch failure when two records but only one fails', async () => {
+		expect.assertions(2);
+		const event = { Records: ['event1', 'event2'] };
+
+		jest
+			.spyOn(EmailRequestProcessor.prototype, 'getRecordS3Objects')
+			.mockReturnValue([{ event: 's3Record1' }] as unknown as S3EventRecord['s3'][]);
+
+		const processSpy = jest.spyOn(EmailRequestProcessor.prototype, 'process');
+		processSpy.mockImplementationOnce(() => Promise.resolve());
+		processSpy.mockImplementationOnce(() => Promise.reject());
+
+		const res = await handler(event, {} as unknown as Context, () => {});
+
+		expect(processSpy).toHaveBeenCalledTimes(2);
+		expect(res.batchItemFailures.length).toBe(1);
+	});
+	it('should have two partial batch failures when two records and both fail', async () => {
+		expect.assertions(2);
+		const event = { Records: ['event1', 'event2'] };
+
+		jest
+			.spyOn(EmailRequestProcessor.prototype, 'getRecordS3Objects')
+			.mockReturnValue([{ event: 's3Record1' }] as unknown as S3EventRecord['s3'][]);
+
+		const processSpy = jest.spyOn(EmailRequestProcessor.prototype, 'process');
+		processSpy.mockImplementation(() => Promise.reject());
+
+		const res = await handler(event, {} as unknown as Context, () => {});
+
+		expect(processSpy).toHaveBeenCalledTimes(2);
+		expect(res.batchItemFailures.length).toBe(2);
+	});
 });
